a809130d5cd10ce02e290e2fce9e7dadd8871955
Wladimir Rocha <wrocha@grupov.com.ve>
2021-11-26 22:13:03 -0400

    [ADD] config formating code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import '../styles/components/app.css'
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello world! on new app</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hello world! on new app</h1>
+    </div>
+  )
 }
 
 export default App
