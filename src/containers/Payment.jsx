873e0e8f3907cb9761f08042a32721629231dd23
Wladimir Rocha <wrocha@grupov.com.ve>
2021-11-29 11:11:12 -0400

    [ADD] payments and success

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import '../styles/components/Payment.css'
 
 const Payment = () => {
   return (
-    <div>
-      <h1>Payment</h1>
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen del Pedido</h3>
+        <button type="button" className="Payment-buttons"> Pago con paypal </button>
+      </div>
     </div>
   )
 }
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Success = () => {
   return (
     <div>
-      <h1>Success</h1>
+      <section className="Succes">
+        <h2>Fulano, gracias por tu compra</h2>
+        <span>Tu pedido llegará muy pronto a la dirección que solicitaste</span>
+      </section>
+      <section className="Success-map">
+        Aqui pondre un mapita :)
+      </section>
     </div>
   )
 }
