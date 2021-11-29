8389c8bd3cd34857b0533ed6ce19833d8f325c14
Wladimir Rocha <wrocha@grupov.com.ve>
2021-11-29 11:03:17 -0400

    [REF] update indent

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import initialState from '../initalState.js'
 import Products from '../components/Products.jsx'
 
-
 const Home = () => {
   return (
     <div>
-      <Products products={initialState.products}/>
+      <Products products={initialState.products} />
     </div>
   )
 }
